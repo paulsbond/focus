29edcc6555fa916da780e4dcab8db23bfcdf5b57
Paul Bond <git@paulsbond.co.uk>
2020-05-29 11:25:43 +0100

    Implemented a simple timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'focus';
+  completedSessions = [];
+  currentTask = "";
+  running = false;
+  remainingSeconds = 1500;
+
+  start() {
+    if (this.currentTask === "") return;
+    console.log(this.currentTask);
+    this.running = true;
+    const interval = setInterval(() => {
+      this.remainingSeconds--;
+      if (this.remainingSeconds === 0) {
+        clearInterval(interval);
+        this.completedSessions.push({
+          task: this.currentTask,
+          endTime: Date.now()
+        });
+        this.running = false;
+        this.remainingSeconds = 1500;
+        this.currentTask = "";
+      }
+    }, 1000);
+  }
 }
