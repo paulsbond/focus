5c6f1b49395ef4074188c7d2be3c56b5374c752a
Paul Bond <git@paulsbond.co.uk>
2020-05-30 14:42:41 +0100

    Added pause/reset and fixed idle bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,32 +8,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   completed = [];
-  currentTask = "";
+  task = "";
   running = false;
-  remainingSeconds = 1500;
+  interval;
+  startTime: number = null;
+  startSeconds = 1500;
+  remainingSeconds = this.startSeconds;
+
+  ngOnInit() {
+    this.storage_get("completed");
+    this.storage_get("task");
+    this.storage_get("startTime");
+    this.storage_get("startSeconds");
+    if (this.startTime !== null) {
+      this.remainingSeconds = this.startSeconds - this.secondsSince(this.startTime);
+      this.start();
+    } else {
+      this.remainingSeconds = this.startSeconds;
+    }
+  }
+
+  secondsSince(time: number) {
+    return Math.floor((Date.now() - time) / 1000);
+  }
+
+  startPause() {
+    if (this.running) {
+      this.pause();
+    } else {
+      this.startTime = Date.now();
+      this.storage_set("startTime");
+      this.start();
+    }
+  }
 
   start() {
-    if (this.currentTask === "") return;
     this.running = true;
-    this.storage_set("currentTask");
-    this.storage_set("remainingSeconds");
-    const interval = setInterval(() => {
-      this.remainingSeconds--;
-      this.storage_set("remainingSeconds");
-      if (this.remainingSeconds === 0) {
-        clearInterval(interval);
-        this.completed.push({
-          task: this.currentTask,
-          endTime: Date.now()
-        });
-        this.storage_set("completed");
-        this.running = false;
-        this.remainingSeconds = 1500;
-        this.currentTask = "";
-        this.storage_del("currentTask");
-        this.storage_del("remainingSeconds");
-      }
-    }, 1000);
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.remainingSeconds--;
+    if (this.remainingSeconds === 0) {
+      this.completed.push({
+        task: this.task,
+        endTime: Date.now()
+      });
+      this.storage_set("completed");
+      this.reset();
+    }
+  }
+
+  pause() {
+    clearInterval(this.interval);
+    this.running = false;
+    this.startTime = null;
+    this.storage_del("startTime");
+    this.startSeconds = this.remainingSeconds;
+    this.storage_set("startSeconds");
+  }
+
+  reset() {
+    if (this.running) {
+      this.pause();
+    }
+    this.startTime = null;
+    this.startSeconds = 1500;
+    this.remainingSeconds = 1500;
+    this.storage_del("startTime");
+    this.storage_del("startSeconds");
   }
 
   clear() {
@@ -40,11 +84,8 @@ export class AppComponent implements OnInit {
     this.storage_del("completed");
   }
 
-  ngOnInit() {
-    this.storage_get("completed");
-    this.storage_get("currentTask");
-    this.storage_get("remainingSeconds");
-    this.start();
+  taskKeyup() {
+    this.storage_set("task");
   }
 
   storage_set(key: string): void {
